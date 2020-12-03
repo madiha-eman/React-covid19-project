5632b942b1cea3246cea77b8b5d28a15f77363bc
madiha-eman <madihaeman1@gmail.com>
2020-12-03 13:15:57 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Api from './components/Api';
 import Api2 from './components/Api2';
+import Class from './components/Class';
 import MediaCard from './components/MediaCard';
 
 function App() {
   return (
     <div>
+      <Class /> 
     <h1>COVID19-APP</h1>
     <Api />
     <Api2 />
--- src/components/Class.js
+++ src/components/Class.js
@@ -1,31 +1,38 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-//  class Class extends Component {
-//     constructor(){
-//         super()
-//  confirmed,
+ class Class extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            people:['confirmed', 'recovered', 'deaths']
+        }
+        console.log(this.state)
 
-//         }
+        get_data =()=>{
+            
+        }
+        }
    
-//     render() {
-//         return (
-//             <>
-//             <Grid container>
-//             <Grid item xs={12} md={4} lg={4}>
-//                 <MediaCard text='Confirmed Cases' value={confirmed}  color='#C53030'/>
-//                 <MediaCard text='Recoverd Cases' value={recovered} color='#97266D'/>
-//                 <MediaCard text='Death Cases' value={deaths} color='#0987A0'/>
+    render() {
+
+        return (
+            <>
+            {/* <Grid container>
+            {/* <Grid item xs={12} md={4} lg={4}>
+                <MediaCard text='Confirmed Cases' value={confirmed}  color='#C53030'/>
+                <MediaCard text='Recoverd Cases' value={recovered} color='#97266D'/>
+                <MediaCard text='Death Cases' value={deaths} color='#0987A0'/>
                 
-//             </Grid>
-//             <Grid item xs={12} md={8} lg={8}>
-//                 <div className='graph'>
+            </Grid>
+            <Grid item xs={12} md={8} lg={8}>
+                <div className='graph'>
            
-//             </div>
-//             </Grid>
+            </div>
+            </Grid>
         
-//             </Grid>
-//     </>
-//         )
-//     }
-// }
-// export default Class;
+            </Grid> */} */}
+    </>
+        )
+    }
+}
+export default Class;
